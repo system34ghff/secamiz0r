4bc58872807e967643815892398d7021c9977bdb
Valery Khabarov <timiriliyev@gmail.com>
2020-03-29 17:07:23 +0900

    Another pack of improvements. * Bump version to 0.9. * Names of parameters changed to more descriptive ones. Added explanations for each parameter. * Separated luma flames and chroma flames. * Restored old behaviour of randomized flames. * Reduced max background noise.

--- secamiz0r.c
+++ secamiz0r.c
@@ -23,11 +23,10 @@ typedef struct secamiz0r_instance_s {
 
     // Parameters set by a user.
     // All values must be in 0..1 range.
-    double reception; /* how much random fires will be emitted */
-    double shift; /* threshold of fires that will be emitted on horizontal edges */
-    double noise; /* controls amount of chroma noise, screws quality of picture */
-
-    bool cross_int;
+    double rnd_factor; /* how much random fires will be emitted */
+    double luma_factor; /* threshold of fires that will be emitted on horizontal edges */
+    double chroma_factor;
+    double simple_noise; /* controls amount of chroma noise, screws quality of picture */
 
     // Input and output frames are in RGBA format.
     gavl_video_format_t format_rgba;
@@ -109,7 +108,7 @@ void f0r_get_plugin_info(f0r_plugin_info_t *info) {
     info->color_model = F0R_COLOR_MODEL_RGBA8888;
     info->frei0r_version = FREI0R_MAJOR_VERSION;
     info->major_version = 0;
-    info->minor_version = 8;
+    info->minor_version = 9;
     info->num_params = 4;
     info->explanation = "Adds so called \"SECAM fire\" to the image.";
 }
@@ -117,24 +116,24 @@ void f0r_get_plugin_info(f0r_plugin_info_t *info) {
 void f0r_get_param_info(f0r_param_info_t* info, int param_index) {
     switch (param_index) {
     case 0:
-        info->name = "Reception";
+        info->name = "Randomization factor";
         info->type = F0R_PARAM_DOUBLE;
-        info->explanation = NULL;
+        info->explanation = "Amount of flames that appear randomly across the frame.";
         break;
     case 1:
-        info->name = "Channel Shift";
+        info->name = "Sharp edge factor";
         info->type = F0R_PARAM_DOUBLE;
-        info->explanation = NULL;
+        info->explanation = "Amount of flames that appear on sharp edges.";
         break;
     case 2:
-        info->name = "Chroma Noise";
+        info->name = "Oversaturation factor";
         info->type = F0R_PARAM_DOUBLE;
-        info->explanation = NULL;
+        info->explanation = "Amount of flames that appear on oversaturated areas.";
         break;
     case 3:
-        info->name = "Luma-chroma interference";
-        info->type = F0R_PARAM_BOOL;
-        info->explanation = NULL;
+        info->name = "Background color noise";
+        info->type = F0R_PARAM_DOUBLE;
+        info->explanation = "Background color noise. Requires a lot of bitrate.";
         break;
     }
 }
@@ -144,9 +143,10 @@ f0r_instance_t f0r_construct(unsigned int width, unsigned int height) {
     inst->width = width;
     inst->height = height;
 
-    inst->reception = 0.95;
-    inst->shift = 0.30;
-    inst->noise = 0.24;
+    inst->rnd_factor = 0.01;
+    inst->luma_factor = 0.30;
+    inst->chroma_factor = 0.48;
+    inst->simple_noise = 0.13;
     
     gavl_video_format_t *rgba = &inst->format_rgba;
     rgba->frame_width = rgba->image_width = inst->width;
@@ -215,16 +215,16 @@ void f0r_set_param_value(f0r_instance_t instance,
     secamiz0r_instance_t* inst = (secamiz0r_instance_t*)instance;
     switch (param_index) {
     case 0:
-        inst->reception = *((double *)param);
+        inst->rnd_factor = *((double *)param);
         break;
     case 1:
-        inst->shift = *((double *)param);
+        inst->luma_factor = *((double *)param);
         break;
     case 2:
-        inst->noise = *((double *)param);
+        inst->chroma_factor = *((double *)param);
         break;
     case 3:
-        inst->cross_int = *((double *)param) >= 0.5;
+        inst->simple_noise = *((double *)param);
         break;
     }
 }
@@ -235,16 +235,16 @@ void f0r_get_param_value(f0r_instance_t instance,
     secamiz0r_instance_t *inst = (secamiz0r_instance_t *)instance;
     switch (param_index) {
     case 0:
-        *((double *)param) = inst->reception;
+        *((double *)param) = inst->rnd_factor;
         break;
     case 1:
-        *((double *)param) = inst->shift;
+        *((double *)param) = inst->luma_factor;
         break;
     case 2:
-        *((double *)param) = inst->noise;
+        *((double *)param) = inst->chroma_factor;
         break;
     case 3:
-        *((double *)param) = inst->cross_int ? 1.0 : 0.0;
+        *((double *)param) = inst->simple_noise;
         break;
     }
 }
@@ -265,13 +265,24 @@ void f0r_update(f0r_instance_t instance, double time,
 // -----------------------------------------------------------------------------
 // SECAM FIRE
 
+#define POW2(x) ((x) * (x))
+#define FRAND() (pcg32_boundedrand_r(&inst->rng, 0xdeadcafe) / (double)0xdeadcafe)
+
+#define RND_CONSTRAINT 0.013
+#define LUMA_DELTA_CONSTRAINT 0.667
+#define CHROMA_DELTA_CONSTRAINT 0.911
+
 void secam_fire(secamiz0r_instance_t *inst, double time) {
     uint8_t *luma = inst->frame_ycbcr->planes[0];
     uint8_t *cb = inst->frame_ycbcr->planes[1];
     uint8_t *cr = inst->frame_ycbcr->planes[2];
 
-    double threshold = 1.0 - inst->shift;
-    double probability = inst->reception * 0.001 + 0.999;
+    double rnd_threshold =
+        1.0 - RND_CONSTRAINT * POW2(inst->rnd_factor);
+    double luma_threshold =
+        1.0 - LUMA_DELTA_CONSTRAINT * inst->luma_factor;
+    double chroma_threshold =
+        1.0 - CHROMA_DELTA_CONSTRAINT * inst->chroma_factor;
 
     for (int cy = 0; cy < inst->cheight - 1; cy++) {
         // Work only on even lines.
@@ -296,15 +307,17 @@ void secam_fire(secamiz0r_instance_t *inst, double time) {
             for (int cx = 0; cx < inst->cwidth; cx++) {
                 int ux = cx + inst->cwidth;
 
+                double magic_random_number = FRAND();
+
                 //
                 // Step 1: add some static chroma noise
-                if (inst->noise) {
+                if (inst->simple_noise) {
                     double amp;
                     double n;
                     int e;
 
-                    // noise increases chroma signal up to 60
-                    amp = inst->noise * 60.0;
+                    // noise increases chroma signal up to 25%
+                    amp = inst->simple_noise * 25.0;
                     e = frame_rand + cy * inst->cwidth + cx;
                     n = get_noise(e, amp, 0.18);
 
@@ -316,7 +329,7 @@ void secam_fire(secamiz0r_instance_t *inst, double time) {
                 // Step 2: Calculate delta value, which is used to create
                 // a fire if there are sharp edges.
                 double luma_delta = 0.0;
-                if (inst->cross_int && cx < inst->cwidth - 1) {
+                if (inst->luma_factor > 0.0 && cx < inst->cwidth - 1) {
                     int x, y, w;
                     int a, a_00, a_01, a_10, a_11;
                     int b, b_00, b_01, b_10, b_11;
@@ -369,11 +382,14 @@ void secam_fire(secamiz0r_instance_t *inst, double time) {
 
                 //
                 // Step 3b: Create a fire if there is need to do it
-                double r = ldexp(pcg32_random_r(&inst->rng), -32);
-                double delta = abs(dst[0][cx] - dst[1][cx]) / 256.0;
-                if (r > probability || (delta + luma_delta) > threshold) {
+                double rnd = ldexp(pcg32_random_r(&inst->rng), -32);
+                double chroma_delta = abs(dst[0][cx] - dst[1][cx]) / 256.0;
+                if (rnd > rnd_threshold
+                ||  chroma_delta > chroma_threshold + magic_random_number
+                ||  luma_delta > luma_threshold + magic_random_number
+                ) {
                     int c = SECAM_FIRE_INTENSITY - dst[p][cx];
-                    fire = r * c;
+                    fire = rnd * c;
                     step = c / (SECAM_FIRE_INTENSITY / 8);
                     if (step < 4) {
                         step = 4;
